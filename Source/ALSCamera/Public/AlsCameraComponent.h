a53d0bde7f5afdf33e91131cab630a56196b88c2
Belfer <154240@buas.nl>
2026-09-11 14:23:16 +0200

    Made CalculateCharacterViewRotation protected

--- Source/ALSCamera/Public/AlsCameraComponent.h
+++ Source/ALSCamera/Public/AlsCameraComponent.h
@@ -126,10 +126,11 @@ public:
 	UFUNCTION(BlueprintPure, Category = "ALS|Camera")
 	void GetViewInfo(FMinimalViewInfo& ViewInfo) const;
 
+protected:
+	virtual FRotator CalculateCharacterViewRotation() const;
+
 private:
 	void TickCamera(float DeltaTime, bool bAllowLag = true);
-
-	virtual FRotator CalculateCharacterViewRotation() const;
 
 	FRotator CalculateCameraRotation(const FRotator& CameraTargetRotation, float DeltaTime, bool bAllowLag) const;
 
